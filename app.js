86a33fe184a8550f1e837645b7f0db6400488b6b
Nitish Bhardwaj <nitish.bhardwaj11@gmail.com>
2015-07-23 14:41:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/news');
+mongoose.connect('mongodb://admin:U64VF6qXvwai@localhost/flappertest');
 
 require('./models/Posts');
 require('./models/Comments');
